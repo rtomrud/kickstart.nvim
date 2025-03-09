f0c2f5cdd466b4126ff8fd5f15ee29967be689b0
Artem Rudenko <rtomrud@gmail.com>
2025-03-09 02:50:05 +0100

    Add custom settings

--- init.lua
+++ init.lua
@@ -670,6 +670,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        ts_ls = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -739,22 +740,22 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = { c = true, cpp = true }
+      --   local lsp_format_opt
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     lsp_format_opt = 'never'
+      --   else
+      --     lsp_format_opt = 'fallback'
+      --   end
+      --   return {
+      --     timeout_ms = 500,
+      --     lsp_format = lsp_format_opt,
+      --   }
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
@@ -762,6 +763,19 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        graphql = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+        sql = { 'sql_formatter' },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        vue = { 'prettierd', 'prettier', stop_after_first = true },
+        yaml = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -846,6 +860,7 @@ require('lazy').setup({
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
           ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-x>'] = require('minuet').make_cmp_map(),
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
@@ -879,32 +894,37 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'nvim_lsp_signature_help' },
+          {
+            name = 'minuet',
+            group_index = 1,
+            priority = 100,
+          },
         },
       }
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+  --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -986,13 +1006,13 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,134 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- GitHub color scheme
+    'projekt0n/github-nvim-theme',
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('github-theme').setup {
+        styles = {
+          comments = { italic = true },
+        },
+      }
+      vim.cmd.colorscheme 'github_dark_default'
+    end,
+  },
+
+  { -- Autocompletion with LLM
+    'milanglacier/minuet-ai.nvim',
+    lazy = true,
+    config = function()
+      require('minuet').setup {
+        cmp = {
+          enable_auto_complete = false,
+        },
+        virtualtext = {
+          auto_trigger_ft = {
+            -- '*'
+          },
+          keymap = {
+            accept = '<C-y>',
+            accept_line = nil,
+            accept_n_lines = nil,
+            prev = '<C-k>',
+            next = '<C-j>',
+            dismiss = nil,
+          },
+        },
+        add_single_line_entry = true,
+        provider = 'openai_fim_compatible',
+        context_window = 4096, -- 32000
+        throttle = 1000,
+        debounce = 250,
+        request_timeout = 15,
+        n_completions = 3,
+        provider_options = {
+          openai_fim_compatible = {
+            model = 'qwen2.5-coder:32b-base-q4_K_M',
+            end_point = 'http://localhost:11434/v1/completions',
+            api_key = 'OLLAMA_API_KEY',
+            name = 'Ollama',
+            stream = true,
+            template = {
+              prompt = function(pref, suff)
+                local prompt_message = ''
+                local vectorcode_cacher = require('vectorcode.config').get_cacher_backend()
+                for _, file in ipairs(vectorcode_cacher.query_from_cache(0)) do
+                  prompt_message = prompt_message .. '<|file_sep|>' .. file.path .. '\n' .. file.document .. '\n'
+                end
+                return prompt_message .. '<|fim_prefix|>' .. pref .. '<|fim_suffix|>' .. suff .. '<|fim_middle|>'
+              end,
+              suffix = false,
+            },
+            optional = {
+              max_tokens = nil,
+              stop = { '\n\n' },
+              top_p = 0.8,
+            },
+          },
+        },
+      }
+    end,
+  },
+
+  { -- Code repository indexing
+    'Davidyz/VectorCode',
+    version = '*', -- optional, depending on whether you're on nightly or release
+    build = 'pipx upgrade vectorcode', -- optional but recommended if you set `version = "*"`
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = true,
+    config = function()
+      require('vectorcode').setup {
+        async_opts = {
+          debounce = 10,
+          events = { 'BufWritePost', 'InsertEnter', 'BufReadPost' },
+          exclude_this = true,
+          n_query = 1,
+          notify = false,
+          query_cb = require('vectorcode.utils').make_surrounding_lines_cb(-1),
+          run_on_register = false,
+          single_job = true,
+          timeout_ms = 15000,
+        },
+        async_backend = 'lsp',
+        exclude_this = true,
+        n_query = 1,
+        notify = true,
+        timeout_ms = 5000,
+        on_setup = {
+          update = false, -- set to true to enable update when `setup` is called.
+        },
+      }
+
+      vim.api.nvim_create_autocmd({ 'LspAttach' }, {
+        callback = function(args)
+          local vectorcode_cacher = require('vectorcode.config').get_cacher_backend()
+          if not vectorcode_cacher.buf_is_registered(args.buf) then
+            vectorcode_cacher.register_buffer(args.buf, {
+              debounce = 10,
+              events = { 'BufWritePost', 'InsertEnter', 'BufReadPost' },
+              exclude_this = true,
+              n_query = 1,
+              notify = false,
+              query_cb = require('vectorcode.utils').make_surrounding_lines_cb(-1),
+              run_on_register = false,
+              single_job = false,
+              timeout_ms = 15000,
+            })
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd({ 'BufDelete' }, {
+        callback = function(args)
+          local vectorcode_cacher = require('vectorcode.config').get_cacher_backend()
+          if vectorcode_cacher.buf_is_registered(args.buf) then
+            vectorcode_cacher.deregister_buffer(args.buf)
+          end
+        end,
+      })
+    end,
+  },
+}
